71e21e95baaec7182a5cea57ae6a67c027732ce1
MushuYoWushu <52196708+mushuyowushu@users.noreply.github.com>
2019-07-19 06:08:11 -0700

    text only renders as intended in intro as intended once
    After the index is changed, the image renders on top of text

--- src/scenes/intro.rs
+++ src/scenes/intro.rs
@@ -21,8 +21,10 @@ use crate::game_logic::scene_type::SceneReturn;
 use quicksilver::prelude::*;
 
 pub struct ElderIntro {
-    intro_img: Asset<Image>,
-    item_img: Asset<Image>,
+    intro_background: Asset<Image>,
+
+    intro_scenes: Vec<Asset<Image>>,
+    curr_scene_index: usize,
     text: Asset<Image>,
 }
 
@@ -30,6 +32,17 @@ impl ElderIntro {
     /// Load the assets and initialise the game
     pub fn new() -> Result<Self> {
         let font_mononoki = "square.ttf";
+        let intro_background = "GCSeamlessBackground800x600.png";
+        //I declare like this because it is a sensible way to organize arbitrary ordered images
+        let intro_scene1 = "FrameSplash1-800x600.png";
+        let intro_scene2 = "FrameSplash2-800x600.png";
+        let intro_scene3 = "FrameSplash3-800x600.png";
+        let intro_scene4 = "FrameSplash4-800x600.png";
+        //Declared here so we can get the length below
+        let scenes = vec![Asset::new(Image::load(intro_scene1)),
+                                            Asset::new(Image::load(intro_scene2)),
+                                            Asset::new(Image::load(intro_scene3)),
+                                            Asset::new(Image::load(intro_scene4))];
 
         //Font Load
         let text_info = Asset::new(Font::load(font_mononoki).and_then( |font| {
@@ -39,13 +52,11 @@ impl ElderIntro {
             )
         }));
 
-        //Image Load
-        let bob = Asset::new(Image::load("PngBob.png"));
-        let game_frame = Asset::new(Image::load("GameFrame800x600.png"));
 
         Ok(Self {
-            intro_img: game_frame,
-            item_img: bob,
+            intro_background: Asset::new(Image::load(intro_background)),
+            intro_scenes: scenes,
+            curr_scene_index: 1,
             text: text_info,
         })
     }
@@ -56,7 +67,13 @@ impl ElderIntro {
         let mut retval = SceneReturn::Good;
 
         if window.keyboard()[Key::Return] == Pressed {
-            retval = SceneReturn::Finished;
+            if self.curr_scene_index < self.intro_scenes.len() - 1 {
+                self.curr_scene_index += 1;
+            } else {
+                self.curr_scene_index = 0;
+                retval = SceneReturn::Finished;
+            }
+
         }
 
         Ok(retval)
@@ -65,8 +82,8 @@ impl ElderIntro {
     /// Draw stuff on the screen
     pub fn draw(&mut self, window: &mut Window) -> Result<()> {
 
-        // Draw the frame
-        self.intro_img.execute(|image| {
+        // Draw the background
+        self.intro_background.execute(|image| {
             window.draw(
                 &image
                     .area()
@@ -76,11 +93,10 @@ impl ElderIntro {
             Ok(())
         })?;
 
-        // Draw bob
-        self.item_img.execute(|image| {
+        // Draw top scene
+        self.intro_scenes[self.curr_scene_index].execute(|image| {
             window.draw(
-                &image
-                    .area()
+                &image.area()
                     .with_center((window.screen_size().x as i32 / 2, window.screen_size().y as i32 / 2)),
                 Img(&image),
             );
@@ -90,9 +106,8 @@ impl ElderIntro {
         // Draw text
         self.text.execute(|image| {
             window.draw(
-                &image
-                    .area()
-                    .translate((2 + 112, window.screen_size().y as i32 - 30 - 84)),
+                &image.area()
+                    .translate((2 + 64, window.screen_size().y as i32 - 30 - 84)),
                 Img(&image),
             );
             Ok(())
