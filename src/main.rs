25d1cf18760ebfe72fbe6bf85c9ae1fd0fb6a514
MushuYoWushu <52196708+mushuyowushu@users.noreply.github.com>
2019-07-30 22:36:10 -0700

    Removed extra license notes, settings do not affect web deployment so they are left as defaults.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-// I am the Elder God. A 3 vs 1 board game made using Quicksilver
-//
-// Copyright (C) 2019  WushuWorks
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 //Declare as 'use'able modules
 mod game_logic;
@@ -37,6 +21,7 @@ fn main() {
     let settings = Settings {
         // If the graphics do need to be scaled (e.g. using
         // `with_center`), blur them. This looks better with fonts.
+
         scale: quicksilver::graphics::ImageScaleStrategy::Blur,
         ..Default::default()
     };
--- src/scenes/outro.rs
+++ src/scenes/outro.rs
@@ -1,20 +1,3 @@
-// I am the Elder God. A 3 vs 1 board game made using Quicksilver
-//
-// Copyright (C) 2019  WushuWorks
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 use crate::game_logic::scene_type::{SceneReturn, PlayerType};
 
 //Resource
